df1b9793b9e4d4c3264b94812112ad519ddb0f71
Andre Gardiner <andre@andorum.com>
2014-05-19 10:59:37 -0500

    Update iSlider.js
    Define iBillboard function -  Parse images that's defined within div ,
    img and a tags  and place them within an array while if greater than 2
    give each a css tag within the array.

--- assets/js/iSlide.js
+++ assets/js/iSlide.js
@@ -1,22 +1,42 @@
 (function ($) {
-	$.fn.iSlide = function (a) {
-		var a = $.extend(true, $.fn.iSlide.defaults, a);
+	$.fn.iSlide = function (c) {
+		var c = $.extend(true, $.fn.iSlide.defaults, c);
 		return this.each(function () {
-			var istart = $(this);
-			var iOptions = {
-				container: istart,
+			var a = $(this);
+			var b = {
+				container: a,
 				transitionInProgress: false,
 				manualTransition: false,
 				pausePresentation: false
 			};
-		 
+			ie6();
+			iBillboard(c, b);
  
 		})
 	};
-	
 	$.fn.iSlide.defaults = {
-		transitionsInterval: 5000,
+		transitionsInterval: 4000,
 		transitionsLength: 600
+	};
+	
+	function iBillboard(e, f) {
+		var g = f.container.find("div");
+		f.images = [];
+		g.each(function (a, b) {
+			var c = jQuery(b).find("img");
+			var d = jQuery(b).find("a");
+			if (c.length >= 2) {
+				f.images.push({
+					title: $(b).find("span").text(),
+					thumb: $(c[0]).attr("src"),
+					large: $(c[1]).attr("src"),
+					link: $(d[0]).attr("href")
+				})
+			}
+		});
+		f.images.current = 0;
+		f.images.next = 1;
+		g.remove()
 	};	
 	
 })(jQuery);
